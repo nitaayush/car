a234151f75546b2bdf221b5222e13ead9cb76a5c
nitaayush <nishant.singh@somaiya.edu>
2018-12-07 15:15:19 +0530

    Added text on Click

--- css.js
+++ css.js
@@ -8,6 +8,7 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
+  var changeColor = document.getElementsByClassName("carName");
   if (n > slides.length) {slideIndex = 1} 
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
@@ -16,6 +17,7 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
+  slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+  
 }
